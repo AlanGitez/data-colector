5aa261c52b88d87ca61760f4624a046dfb57677d
alan <abenitez@pigmalion.com.ar>
2023-08-12 15:59:53 -0300

    API class creada.

---
+++ src/main/java/org/datacol/core/API.java
@@ -0,0 +1,12 @@
+package org.datacol.core;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+
+public class API extends AbstractVerticle {
+    private static final Logger logger = LoggerFactory.getLogger(AbstractVerticle.class);
+
+
+
+}
